9f8d2417fa2b353df1058efc706991aec484e634
Deegan Acevedo <deeganacevedo@my.whccd.edu>
2026-01-16 18:38:24 -0800

    Added items 1-10

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -5,7 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("100 Things About Me");
-            Console.WriteLine("1.");
+            Console.WriteLine("1. I am 19 years old");
+            Console.WriteLine("2. I live in Hanford");
+            Console.WriteLine("3. This is my 3rd semester at Lemoore College");
+            Console.WriteLine("4. I graduated from Hanford West High School");
+            Console.WriteLine("5. I enjoy playing video games");
+            Console.WriteLine("6. I enjoy learning about many topics");
+            Console.WriteLine("7. I like to learn about general space");
+            Console.WriteLine("8. I like to learn about space flight");
+            Console.WriteLine("9. I like to learn about history");
+            Console.WriteLine("10. I don't like remembering exact dates and names");
         }
     }
 }
